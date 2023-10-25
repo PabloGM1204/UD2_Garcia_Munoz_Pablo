93a47bb8bb99d0fa44f53774538e24ec373e835c
Pablo García Muñoz <115071457+PabloGM1204@users.noreply.github.com>
2023-10-25 12:27:31 +0200

    Tarea 1 v2.0

---
+++ UD2T1GarcíaMuñozPablo/UD2T1GarcíaMuñozPablo/Empleado.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public class Empleado{
+    // ---------Variables de un empleado---------
+    string nombre;
+    string apellido1;
+    string apellido2;
+    int edad;
+    string nif;
+    double salario;
+
+    // ---------Constructores---------
+    public Empleado() { } // Constructor vacio
+    public Empleado(string nombre, string apellido1, string apellido2, int edad, string nif, double salario)
+    { // Constructor para recibir parametros
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+        this.salario = salario;
+        this.nif = nif;
+    }
+
+    // ---------Getters y Setters---------
+    public string Nombre { get; set; }
+    public string Apellido1 { get; set; }
+    public string Apellido2 { get; set; }
+    public int Edad { get; set; }
+    public string Nif { get; set; }
+    public double Salario { get; set; }
+
+    // ---------Variables de la clase---------
+    string euroSymbol = "\u20AC";
+
+    // ---------Métodos---------
+    public void MostrarEmpleado(Empleado empleado)
+    { // Muestra un resumen de los datos introducidos del empleado
+        Console.WriteLine("********Resumen********\n" +
+            $"Nombre: " + empleado.Nombre + " " + empleado.Apellido1 + " " + empleado.Apellido2+"\n" +
+            $"Edad: " + empleado.Edad + " años\n" +
+            $"NIF: " + empleado.Nif + "\n" +
+            $"Salario: " + empleado.Salario + " €\n");
+    }
+
+    public void ActualizarSalario(double actSalar)
+    { // Recoge el valor del nuevo salario
+        Salario = actSalar;
+    }
+
+    public void ActualizarNif(Empleado empleadoo)
+    { // Pide un dato para actulizar el NIF
+        Console.Write("Dígame su NIF: ");
+        empleadoo.Nif = Console.ReadLine();
+        Console.WriteLine("NIF actualizado");
+    }
+
+    public string MostrarNombre(Empleado empleado)
+    { // Muestra el nombre completo del empleado
+        string nombre = empleado.Nombre + " " + empleado.Apellido1 + " " + empleado.Apellido2;
+        return nombre;
+    }
+    
+    public void MostrarNif(Empleado empleado)
+    { // Muestra el NIF del empleado
+        Console.WriteLine("Su NIF es:" + empleado.Nif);
+    }
+
+    public void MostrarEdad(Empleado empleado)
+    { // Muestra la edad del empleado
+        Console.WriteLine("La edad del empleado es: " + empleado.Edad);
+    }
+
+}
--- UD2T1GarcíaMuñozPablo/UD2T1GarcíaMuñozPablo/Program.cs
+++ UD2T1GarcíaMuñozPablo/UD2T1GarcíaMuñozPablo/Program.cs
@@ -1,28 +1,29 @@
-﻿namespace UD2T1GarcíaMuñozPablo
-{
+﻿namespace UD2T1GarcíaMuñozPablo{
     ///////////////////////////////////////////
     // Tarea: UD2T1
     // Alumno: Pablo García Muñoz
     // Curso: 2023/2024
     ///////////////////////////////////////////
-    internal class Program
-    {
+    internal class Program{
         static void Main(string[] args){
+            // Poder mostrar el simbolo del euro
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             // Variable que guarda la opción seleccionada y en el caso salir del bucle
             int op = 0;
             // Empleado
             Empleado empleado = new Empleado();
             do{
-            // Mostrar el menú de opciones de la consola al iniciar el programa
-            Console.WriteLine("Elige una de las siguientes opciones: \n" +
-                                "0 - Salir\n" +
-                                "1 - Crear empleado\n" +
-                                "2 - Actualizar salario\n" +
-                                "3 - Nombre completo\n" +
-                                "4 - Obtener NIF\n" +
-                                "5 - Actualizar NIF\n" +
-                                "6 - Obtener Edad\n" +
-                                "7 - Mostrar\n");
+                // Mostrar el menú de opciones de la consola al iniciar el programa
+                Console.Write("0 - Salir\n" +
+                              "1 - Crear empleado\n" +
+                              "2 - Actualizar salario\n" +
+                              "3 - Mostrar Nombre\n" +
+                              "4 - Mostrar NIF\n" +
+                              "5 - Actualizar NIF\n" +
+                              "6 - Mostrar edad\n" +
+                              "7 - Mostrar empleado\n" +
+                              "Introduzca su opción: ");
+
             // Lee la opción que elige el usuario
             op = int.Parse(Console.ReadLine());
             // Switch para que haga una cosa u otra dependiendo del número
@@ -34,121 +35,63 @@
                         break;
                     // Caso para crear un nuevo empleado
                     case 1:
+                        // Preguntar al profesor por la creación
                         Console.WriteLine("********Creación de empleado********");
-                        Console.WriteLine("Nombre: ");
+                        Console.Write("Nombre: ");
                         string nombre = Console.ReadLine();
                         empleado.Nombre = nombre;
-                        Console.WriteLine("Primer apellido: ");
+                        Console.Write("Primer apellido: ");
                         string apellido1 = Console.ReadLine();
                         empleado.Apellido1 = apellido1;
-                        Console.WriteLine("Segundo apellido: ");
+                        Console.Write("Segundo apellido: ");
                         string apellido2 = Console.ReadLine();
                         empleado.Apellido2 = apellido2;
-                        Console.WriteLine("Edad: ");
+                        Console.Write("Edad: ");
                         int edad = int.Parse(Console.ReadLine());
                         empleado.Edad = edad;
-                        Console.WriteLine("NIF: ");
+                        Console.Write("NIF: ");
                         string nif = Console.ReadLine();
                         empleado.Nif = nif;
-                        Console.WriteLine("Salario: ");
+                        Console.Write("Salario: ");
                         double salario = double.Parse(Console.ReadLine());
                         empleado.Salario = salario;
-                        empleado.ResumenEmpleado();
+                        empleado.MostrarEmpleado(empleado);
                         break;
 
                     // Caso para actualizar el salario
                     case 2:
-                        empleado.ActualizarSalario();
+                        Console.WriteLine("********Actualización de salario********");
+                        Console.Write("Escriba su nuevo salario: ");
+                        double actSalar = double.Parse(Console.ReadLine());
+                        empleado.ActualizarSalario(actSalar);
                         break;
                     
                     // Caso para mostrar el nombre
                     case 3:
-                        string nom = empleado.MostrarNombre();
-                        Console.WriteLine("Su nombre es: "+nom + "\n");
+                        Console.WriteLine("El nombre es: " + empleado.MostrarNombre(empleado));
                         break;
 
                     // Caso para mostrar el NIF
                     case 4:
-                        Console.WriteLine("Su NIF es:"+empleado.Nif + "\n");
+                        empleado.MostrarNif(empleado);
                         break;
 
                     // Caso para actualizar el NIF
                     case 5:
-                        empleado.ActualizarNif();
+                        empleado.ActualizarNif(empleado);
                         break;
 
                     // Caso para mostrar la edad
                     case 6:
-                        Console.WriteLine("Su edad es:"+empleado.Edad+"\n");
+                        empleado.MostrarEdad(empleado);
                         break;
 
                     // Caso para mostrar todos los datos del empleado
                     case 7:
-                        empleado.ResumenEmpleado();
+                        empleado.MostrarEmpleado(empleado);
                         break;
                 }
             } while (op != 0);
         }
-    }
-
-
-
-
-    // Clase del empleado el cual usaremos
-    class Empleado{
-        // ---------Variables de un empleado---------
-        string nombre;
-        string apellido1;
-        string apellido2;
-        int edad;
-        string nif;
-        double salario;
-
-        // ---------Constructores---------
-        public Empleado() { } // Constructor vacio
-        public Empleado(string nombre, string apellido1, string apellido2, int edad, string nif, double salario){ // Constructor para recibir parametros
-            this.nombre = nombre;
-            this.apellido1 = apellido1;
-            this.apellido2 = apellido2;
-            this.edad = edad;
-            this.salario = salario;
-            this.nif = nif;
-        }
-
-        // ---------Getters y Setters---------
-        public string Nombre { get; set; }
-        public string Apellido1 { get; set; }
-        public string Apellido2 { get; set; }
-        public int Edad { get; set; }
-        public string Nif { get; set; }
-        public double Salario { get; set; }
-
-        // ---------Métodos---------
-        public void ResumenEmpleado(){ // Muestra un resumen de los datos introducidos del empleado
-            Console.WriteLine("********Resumen********\n" +
-                $"Nombre: {Nombre} {Apellido1} {Apellido2}\n" +
-                $"Edad: {Edad}\n" +
-                $"NIF: {Nif}\n"+
-                $"Salario: {Salario}\n");
-        }
-
-        public void ActualizarSalario(){ // Pide un dato para actulizar el salario
-            Console.WriteLine("Dígame su salario: ");
-            Salario = double.Parse(Console.ReadLine());
-            Console.WriteLine("Salario actualizado");
-        }
-
-        public void ActualizarNif(){ // Pide un dato para actulizar el NIF
-            Console.WriteLine("Dígame su NIF: ");
-            Nif = Console.ReadLine();
-            Console.WriteLine("NIF actualizado");
-        }
-
-        public string MostrarNombre(){ // Muestra el nombre completo del empleado
-            return(Nombre+" "+Apellido1+" "+Apellido2+"\n");
-        }
-
-
-
     }
 }
