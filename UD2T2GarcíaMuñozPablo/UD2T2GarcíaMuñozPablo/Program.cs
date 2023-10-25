44e684154a993aa5aab6b507a1b0935493f95f7e
Pablo García Muñoz <115071457+PabloGM1204@users.noreply.github.com>
2023-10-25 12:24:39 +0200

    Tarea 1 v2.0

---
+++ UD2T2GarcíaMuñozPablo/UD2T2GarcíaMuñozPablo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UD2T2GarcíaMuñozPablo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
